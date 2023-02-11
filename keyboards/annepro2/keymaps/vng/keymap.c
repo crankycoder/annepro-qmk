26b1b1878e0e2296cb071dd63dec0fa64674ef97
Victor Ng <victor@crankycoder.com>
2023-02-11 01:45:44 -0500

    FN1: (T, W) => (New Tab, Close Tab) FN1: Arrow keys in bottom right

--- keyboards/annepro2/keymaps/vng/keymap.c
+++ keyboards/annepro2/keymaps/vng/keymap.c
@@ -80,11 +80,11 @@ enum anne_pro_layers {
   *
   */
  [FN1] = LAYOUT_60_ansi( /* FN1 */
-    KC_GRV  ,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,
-    KC_LBRC , _______, KC_UP,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  KC_RBRC,
-    LSFT(KC_LBRC), KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, _______, _______, LSFT(KC_RBRC),
-    SC_LSPO ,          KC_VOLU, KC_VOLD, KC_MUTE, _______, _______, _______, _______, _______, _______, _______, SC_RSPC,
-    _______ , _______, _______,                            _______,                   _______, _______, _______, _______ 
+    KC_GRV          ,  KC_F1,   KC_F2   ,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,
+    KC_LBRC         , _______, LCTL(KC_W),   _______, _______, LCTL(KC_T), _______, _______, _______, _______, _______, _______, _______,  KC_RBRC,
+    LSFT(KC_LBRC)   , _______, _______  , _______, _______, _______, KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, _______, _______, LSFT(KC_RBRC),
+    SC_LSPO         ,          KC_VOLU  , KC_VOLD, KC_MUTE, _______, _______, _______, _______, _______, _______, KC_UP, SC_RSPC,
+    _______         , _______, _______  ,                            _______,                   KC_LEFT, KC_DOWN, KC_RIGHT, _______ 
 ),
   /*
   * Layer FN2
