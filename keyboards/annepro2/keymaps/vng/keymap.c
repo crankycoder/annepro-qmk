99974a8a3068df532ba4833ebed1bead15354569
Victor Ng <victor@crankycoder.com>
2023-02-11 01:13:03 -0500

    Remove arrow keys and enable SpaceCadet
    On FN1
    
    Shift is ()
    Capslock/Enter is {}
    Tab/Backslash is  []

--- keyboards/annepro2/keymaps/vng/keymap.c
+++ keyboards/annepro2/keymaps/vng/keymap.c
@@ -17,8 +17,8 @@
 #include QMK_KEYBOARD_H
 
 enum {
-    TD_LALT_CTRL_PGUP,
-    TD_RALT_CTRL_PGDN
+    TD_LCTL_CTRL_PGUP,
+    TD_RCTL_CTRL_PGDN
 };
 
 
@@ -61,8 +61,8 @@ enum anne_pro_layers {
     KC_GRV                  , KC_1      , KC_2                  , KC_3              , KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC,
     KC_TAB                  , KC_Q      , KC_W                  , KC_E              , KC_R, KC_T, KC_Y,   KC_U, KC_I, KC_O,    KC_P,             KC_LBRC,          KC_RBRC,       KC_BSLS,
     MT(MOD_LGUI, KC_ESC)    , KC_A      , KC_S                  , KC_D              , KC_F, KC_G, KC_H, KC_J, KC_K,    KC_L,   KC_SCLN, KC_QUOT, KC_ENT,
-    KC_LSFT                 , KC_Z      , KC_X                  , KC_C              , KC_V, KC_B,   KC_N, KC_M, KC_COMM, KC_DOT,           KC_SLSH,          RSFT_T(KC_UP),
-    KC_LCTL                 , KC_LGUI   , TD(TD_LALT_CTRL_PGUP) , LT(FN1, KC_SPC)   , TD(TD_RALT_CTRL_PGDN), LT(FN1, KC_LEFT), LT(FN2 ,KC_DOWN), RCTL_T(KC_RGHT)
+    KC_LSFT                 , KC_Z      , KC_X                  , KC_C              , KC_V, KC_B,   KC_N, KC_M, KC_COMM, KC_DOT,           KC_SLSH,          KC_RSFT,
+    TD(TD_LCTL_CTRL_PGUP)   , KC_LGUI   , KC_LALT               , LT(FN1, KC_SPC)   , KC_RALT, FN1, FN2, TD(TD_RCTL_CTRL_PGDN)
 ),
   /*
   * Layer FN1
@@ -80,11 +80,11 @@ enum anne_pro_layers {
   *
   */
  [FN1] = LAYOUT_60_ansi( /* FN1 */
-    KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,
-    _______, _______, KC_UP,   _______, _______, _______, _______, _______, _______, _______, KC_PSCR, KC_HOME, KC_END,  _______,
-    _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, KC_PGUP, KC_PGDN, _______,
-    _______,          KC_VOLU, KC_VOLD, KC_MUTE, _______, _______, _______, _______, _______, KC_INS,  KC_DEL,  _______,
-    _______, _______, _______,                            _______,                   _______, _______, MO(FN2), _______
+    KC_GRV  ,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,
+    KC_LBRC , _______, KC_UP,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  KC_RBRC,
+    LSFT(KC_LBRC), KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, _______, _______, LSFT(KC_RBRC),
+    SC_LSPO ,          KC_VOLU, KC_VOLD, KC_MUTE, _______, _______, _______, _______, _______, _______, _______, SC_RSPC,
+    _______ , _______, _______,                            _______,                   _______, _______, _______, _______ 
 ),
   /*
   * Layer FN2
@@ -152,7 +152,7 @@ bool led_update_user(led_t leds) {
 }
 
 qk_tap_dance_action_t tap_dance_actions[] = {
-    [TD_LALT_CTRL_PGUP]   = ACTION_TAP_DANCE_DOUBLE(KC_LALT, LCTL(KC_PGUP)),
-    [TD_RALT_CTRL_PGDN] = ACTION_TAP_DANCE_DOUBLE(KC_RALT, LCTL(KC_PGDN))
+    [TD_LCTL_CTRL_PGUP] = ACTION_TAP_DANCE_DOUBLE(KC_LCTL, LCTL(KC_PGUP)),
+    [TD_RCTL_CTRL_PGDN] = ACTION_TAP_DANCE_DOUBLE(KC_RCTL, RCTL(KC_PGDN))
 };
 
