3234389f07a50d168edd4fa44acf6cf3cf6bd30f
Victor Ng <victor@crankycoder.com>
2023-02-13 12:02:22 -0500

    esc/ctrl and spacefn:esc/cmd give me better window management

--- keyboards/annepro2/keymaps/vng/keymap.c
+++ keyboards/annepro2/keymaps/vng/keymap.c
@@ -60,14 +60,14 @@ enum anne_pro_layers {
  [BASE] = LAYOUT_60_ansi( /* Base */
     KC_GRV                  , KC_1      , KC_2                  , KC_3              , KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC,
     KC_TAB                  , KC_Q      , KC_W                  , KC_E              , KC_R, KC_T, KC_Y,   KC_U, KC_I, KC_O,    KC_P,             KC_LBRC,          KC_RBRC,       KC_BSLS,
-    MT(MOD_LGUI, KC_ESC)    , KC_A      , KC_S                  , KC_D              , KC_F, KC_G, KC_H, KC_J, KC_K,    KC_L,   KC_SCLN, KC_QUOT, KC_ENT,
+    MT(MOD_LCTL, KC_ESC)    , KC_A      , KC_S                  , KC_D              , KC_F, KC_G, KC_H, KC_J, KC_K,    KC_L,   KC_SCLN, KC_QUOT, KC_ENT,
     SC_LSPO                 , KC_Z      , KC_X                  , KC_C              , KC_V, KC_B,   KC_N, KC_M, KC_COMM, KC_DOT,           KC_SLSH,          SC_RSPC,
     TD(TD_LCTL_CTRL_PGUP)   , KC_LGUI   , KC_LALT               , LT(FN1, KC_SPC)   , KC_RALT, MO(FN1), LT(FN2 , KC_RGUI), TD(TD_RCTL_CTRL_PGDN)
 ),
   /*
   * Layer FN1
   * ,-----------------------------------------------------------------------------------------.
-  * |  `  |  F1 |  F2 |  F3 |  F4 |  F5 |  F6 |  F7 |  F8 |  F9 | F10 | F11 | F12 |  DELETE   |
+  * | Esc |  F1 |  F2 |  F3 |  F4 |  F5 |  F6 |  F7 |  F8 |  F9 | F10 | F11 | F12 |  DELETE   |
   * |-----------------------------------------------------------------------------------------+
   * | Tab    |  q  | UP  |  e  |  r  |  t  |  y  |  u  |  i  |  o  | PS | HOME | END |   \    |
   * |-----------------------------------------------------------------------------------------+
@@ -80,10 +80,10 @@ enum anne_pro_layers {
   *
   */
  [FN1] = LAYOUT_60_ansi( /* FN1 */
-    KC_GRV          ,  KC_F1,   KC_F2   ,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,
-    KC_LBRC         , _______, LCTL(KC_W),   _______, _______, LCTL(KC_T), _______, _______, _______, _______, _______, _______, _______,  KC_RBRC,
-    LSFT(KC_LBRC)   , _______, _______  , _______, _______, _______, KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, _______, _______, LSFT(KC_RBRC),
-    _______         ,          KC_VOLD  , KC_VOLU, KC_MUTE, _______, _______, _______, _______, _______, _______, KC_UP, _______,
+    KC_ESC          , KC_F1     ,   KC_F2   , KC_F3     ,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,
+    LSFT(KC_LBRC)   , _______   , _______   , _______   , _______, _______, _______, _______, _______, _______, _______, _______, _______,  RSFT(KC_RBRC),
+    MT(MOD_LGUI, KC_LBRC), _______, _______ , _______   , _______, _______, KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, _______, _______, KC_RBRC,
+    _______         , KC_VOLD   , KC_VOLU   , KC_MUTE   , _______, _______, _______, _______, _______, _______, KC_UP, _______,
     _______         , _______, _______  ,                            _______,                   KC_LEFT, KC_DOWN, KC_RIGHT, _______ 
 ),
   /*
